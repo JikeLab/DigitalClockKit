bf53f005a84d40fc9f863951f745a6412c7c3992
Atsushi Jike <a_jike@learnie-inc.com>
2023-08-20 18:12:24 +0900

    Add initial files

---
+++ LiquidCrystalDisplayUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiquidCrystalDisplayUI
+//
+//  Created by Atsushi Jike on 2023/08/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LiquidCrystalDisplayUI/LiquidCrystalDisplayUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidCrystalDisplayUIApp.swift
+//  LiquidCrystalDisplayUI
+//
+//  Created by Atsushi Jike on 2023/08/20.
+//
+
+import SwiftUI
+
+@main
+struct LiquidCrystalDisplayUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
